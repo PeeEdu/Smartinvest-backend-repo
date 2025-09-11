7a3df012352f2477bad74cf412a298359c87a4df
Ainstenpai <pedro.edu2167@gmail.com>
2025-09-11 15:07:19 -0300

    initialized smart-invest-api

---
+++ src/main/java/br/com/smartinvest/smart_invest_api/SmartInvestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.smartinvest.smart_invest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInvestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInvestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/smartinvest/smart_invest_api/SmartInvestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartinvest.smart_invest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInvestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
