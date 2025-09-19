9b3bca2583a15009e81b29d7846a41c7bfa7fa49
Pedro Eduardo <pedro.edu2167@gmail.com>
2025-09-19 08:46:50 -0300

    Update UsuarioController.java

--- src/main/java/br/com/smartinvest/smart_invest_api/controller/UsuarioController.java
+++ src/main/java/br/com/smartinvest/smart_invest_api/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package br.com.smartinvest.smart_invest_api.controller;
 import br.com.smartinvest.smart_invest_api.DTO.request.UsuarioRequestDTO;
 import br.com.smartinvest.smart_invest_api.DTO.response.BaseResponse;
 import br.com.smartinvest.smart_invest_api.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class UsuarioController {
     }
 
     @GetMapping
+    @Operation(summary = "Listar Usuarios")
     public ResponseEntity<BaseResponse> getUsuarios() {
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.builder()
                 .message("Usuarios Listados")
@@ -27,6 +29,7 @@ public class UsuarioController {
     }
 
     @PostMapping
+    @Operation(summary = "Salvar Usuario")
     public ResponseEntity<BaseResponse> saveUsuario(@RequestBody UsuarioRequestDTO usuarioRequestDTO) {
         return ResponseEntity.status(HttpStatus.OK).
                 body(BaseResponse.builder()
@@ -38,6 +41,7 @@ public class UsuarioController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar Usuario")
     public ResponseEntity<BaseResponse> updateUsuario(@PathVariable Long id, @RequestBody UsuarioRequestDTO usuarioRequestDTO) {
         return ResponseEntity.status(HttpStatus.OK).
                 body(BaseResponse.builder()
