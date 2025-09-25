7119ce9e0abc1a257f14acfa79f413945fc37718
Matheus Reis <matheusdanireis@gmail.com>
2025-09-25 11:10:29 -0300

    criar enum TipoPerfil com os perfis Conservador, Moderado e Arrojado

--- src/main/java/br/com/smartinvest/smart_invest_api/enums/TipoPerfil.java
+++ src/main/java/br/com/smartinvest/smart_invest_api/enums/TipoPerfil.java
@@ -1,7 +1,7 @@
 package br.com.smartinvest.smart_invest_api.enums;
 
 public enum TipoPerfil {
-        CONSERVADOR, //0
-        MODERADO, //1
-        ARROJADO; //2
+        CONSERVADOR,
+        MODERADO,
+        ARROJADO;
 }
