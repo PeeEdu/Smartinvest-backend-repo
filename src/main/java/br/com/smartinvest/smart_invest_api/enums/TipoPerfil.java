44525fdd0eea311a623c856efa1374128a8c5446
Matheus Reis <matheusdanireis@gmail.com>
2025-09-25 11:33:08 -0300

    enum topo de perfil

--- src/main/java/br/com/smartinvest/smart_invest_api/enums/TipoPerfil.java
+++ src/main/java/br/com/smartinvest/smart_invest_api/enums/TipoPerfil.java
@@ -1,7 +1,7 @@
 package br.com.smartinvest.smart_invest_api.enums;
 
 public enum TipoPerfil {
-        CONSERVADOR,
-        MODERADO,
-        ARROJADO;
+        CONSERVADOR, //0
+        MODERADO, //1
+        ARROJADO; //2
 }
