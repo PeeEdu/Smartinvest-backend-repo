07411528b2f9da6fb4a910da1f73c90a12ed609e
Matheus Reis <matheusdanireis@gmail.com>
2025-09-25 23:54:02 -0300

    Criar SimulcaoController

---
+++ src/main/java/br/com/smartinvest/smart_invest_api/repository/SimulacaoRepository.java
@@ -0,0 +1,17 @@
+package br.com.smartinvest.smart_invest_api.repository;
+
+
+
+import br.com.smartinvest.smart_invest_api.model.Simulacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SimulacaoRepository extends JpaRepository<Simulacao, Long> {
+
+    Optional<Simulacao> findByNumeroSimulacao(String numeroSimulacao);
+
+}
