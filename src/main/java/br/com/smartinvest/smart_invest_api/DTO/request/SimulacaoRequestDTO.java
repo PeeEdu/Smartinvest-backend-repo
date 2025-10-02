65d177515578e7aec574bca7f24b7b47d8bf8604
Pedro Eduardo <pedro.edu2167@gmail.com>
2025-10-02 08:43:06 -0300

    Create Exception

--- src/main/java/br/com/smartinvest/smart_invest_api/DTO/request/SimulacaoRequestDTO.java
+++ src/main/java/br/com/smartinvest/smart_invest_api/DTO/request/SimulacaoRequestDTO.java
@@ -2,15 +2,23 @@ package br.com.smartinvest.smart_invest_api.DTO.request;
 
 import br.com.smartinvest.smart_invest_api.enums.TipoInvestimento;
 import br.com.smartinvest.smart_invest_api.enums.TipoUsuario;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 
 @Builder
 public record SimulacaoRequestDTO(
+        @NotNull
         BigDecimal valorInicial,
+
+        @NotNull
         TipoInvestimento tipoInvestimento,
+
+        @NotNull
         Double taxaJuros,
+
+        @NotNull
         TipoUsuario tipoUsuario
 ) {
 }
--- src/main/java/br/com/smartinvest/smart_invest_api/controller/SimulacaoController.java
+++ src/main/java/br/com/smartinvest/smart_invest_api/controller/SimulacaoController.java
@@ -3,11 +3,16 @@ package br.com.smartinvest.smart_invest_api.controller;
 
 import br.com.smartinvest.smart_invest_api.DTO.request.SimulacaoRequestDTO;
 import br.com.smartinvest.smart_invest_api.DTO.response.BaseResponse;
+import br.com.smartinvest.smart_invest_api.DTO.response.SimulacaoResponseDTO;
+import br.com.smartinvest.smart_invest_api.exceptions.RequestIsNullException;
 import br.com.smartinvest.smart_invest_api.service.SimulacaoService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/simulacao")
@@ -20,18 +25,25 @@ public class SimulacaoController {
 
     @GetMapping
     @Operation(summary = "Listar Simulações")
-    public ResponseEntity<BaseResponse> getSimulcao() {
+    public ResponseEntity<BaseResponse> getAllSimulcao() {
+        List<SimulacaoResponseDTO> simulacaoResponseDTO = simulacaoService.getAllSimulacoes();
+
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.builder()
                 .message("Simulaçõe Listadas")
                 .status(HttpStatus.OK)
-                .data(simulacaoService.getAllSimulacoes())
+                .data(simulacaoResponseDTO)
                 .build()
         );
     }
 
     @PostMapping
     @Operation(summary = "Salvar Simulações")
-    public ResponseEntity<BaseResponse> saveSimulacao(@RequestBody SimulacaoRequestDTO simulacaoRequestDTO) {
+    public ResponseEntity<BaseResponse> saveSimulacao(@RequestBody @Valid SimulacaoRequestDTO simulacaoRequestDTO) {
+
+        if (simulacaoRequestDTO == null) {
+            throw new RequestIsNullException("SimulacaoRequestDTO");
+        }
+
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.builder()
                 .message("Simulaçõe Salva")
                 .status(HttpStatus.OK)
