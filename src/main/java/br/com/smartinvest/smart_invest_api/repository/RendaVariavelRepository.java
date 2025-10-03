6c7cd72435dcebbcfca40af9321020ecbfcf1e8d
Pedro Eduardo <106138432+PeeEdu@users.noreply.github.com>
2025-10-02 23:10:51 -0300

    Revert "Revert "Add RendaVariavel entity and repository import""

--- src/main/java/br/com/smartinvest/smart_invest_api/repository/RendaVariavelRepository.java
+++ src/main/java/br/com/smartinvest/smart_invest_api/repository/RendaVariavelRepository.java
@@ -1,5 +1,6 @@
 package br.com.smartinvest.smart_invest_api.repository;
 
+import br.com.smartinvest.smart_invest_api.model.RendaVariavel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
